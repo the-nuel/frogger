ed74aa2cf6b3562beee524f16c1e3792b8269172
Tom Davies <tom@thenuel.com>
2019-07-20 23:27:59 +0100

    Fixes missing types for ExecuteStep constructor

--- src/steps/ExecuteStep.ts
+++ src/steps/ExecuteStep.ts
@@ -2,6 +2,11 @@ import { Step } from './Step';
 
 export type ExecuteFunction<TState> = (state: TState) => Promise<TState>;
 
+type StepOptions<TState> = {
+    name: string;
+    execute: ExecuteFunction<TState>;
+};
+
 /**
  * A simple step that executes a function to transform the state.
  */
@@ -9,7 +14,7 @@ export class ExecuteStep<TState> implements Step<TState> {
     name: string;
     protected execute: ExecuteFunction<TState>;
 
-    constructor({ name, execute }) {
+    constructor({ name, execute }: StepOptions<TState>) {
         this.name = name;
         this.execute = execute;
     }
